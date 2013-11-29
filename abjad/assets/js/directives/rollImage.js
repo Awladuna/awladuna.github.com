afb9e3aa8429b7374d854ae2f8319714f99fda3f
Obada Kadri <okadri@ucdavis.edu>
2013-11-29 11:55:28 -0800

    Play hint when big image is clicked in 'search' activity

--- abjad/assets/js/directives/rollImage.js
+++ abjad/assets/js/directives/rollImage.js
@@ -6,6 +6,14 @@ AbjadApp.directive('rollImage', function() {
         if (s.length < 2) s = "0" + s;
         element.attr('src', 'assets/img/letter/' + s + '/1.png');
       });
+
+      scope.playHint = function () {
+        var s = scope.currentWord + "";
+        if (s.length < 2) s = "0" + s;
+        var snd = new Audio("assets/raw/letter/" + s + ".ogg");
+        snd.load();
+        snd.play();
+      };
     }
   };
 });
