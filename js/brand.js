aa0c1804c6a28d38d4a1cdfd1fbc00dff960a4f0
Obada Kadri <abu3antar@gmail.com>
2015-11-28 12:21:07 -0800

    Initial Jekyll work

--- js/brand.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('.brand-region').append('<div class="brand">obadakadri.com</brand>');
-	$('.brand').click(function(e){
-		e.preventDefault()
-		$(location).attr('href','http://obadakadri.com');
-	});
-});
